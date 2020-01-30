0699dde1a423884f47c4d712b0077948d1718417
Erik Rasmussen <rasmussenerik@gmail.com>
2020-01-30 09:23:20 +0100

    Added podcorn prefix

--- src/utils/prefixMp3.ts
+++ src/utils/prefixMp3.ts
@@ -1,4 +1,8 @@
-const prefixes = ['dts.podtrac.com/redirect.mp3/', 'chtbl.com/track/73173/']
+const prefixes = [
+  'dts.podtrac.com/redirect.mp3/',
+  'chtbl.com/track/73173/',
+  'pdcn.co/e/',
+]
 
 export const prefixMp3 = (file: string) =>
   `https://${prefixes.reduce(
