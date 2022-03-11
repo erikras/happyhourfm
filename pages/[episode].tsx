47f21d6138f8cd60b5ecbbf693e67f037731c728
Erik Rasmussen <rasmussenerik@gmail.com>
2022-03-11 09:59:38 +0100

    Video link to patreon

--- pages/[episode].tsx
+++ pages/[episode].tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from '../components/Head'
-import {Box} from 'grommet'
-import Listen from '../components/Listen'
-import {FMType} from '../components/ShowList'
-import Footer from '../components/Footer'
-import contents from '../util/content'
-import {GetStaticPaths, GetStaticProps} from 'next'
-import {DownloadBar} from '../components/DownloadBar'
-import Player from '../components/Player'
-import ShowNotes from '../components/ShowNotes'
-import {defaultImage, url} from '../util/constants'
-import YouTube from '../components/YouTube'
+import React from "react";
+import Link from "next/link";
+import Head from "../components/Head";
+import { Box } from "grommet";
+import Listen from "../components/Listen";
+import { FMType } from "../components/ShowList";
+import Footer from "../components/Footer";
+import contents from "../util/content";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { DownloadBar } from "../components/DownloadBar";
+import Player from "../components/Player";
+import ShowNotes from "../components/ShowNotes";
+import { defaultImage, url } from "../util/constants";
+import YouTube from "../components/YouTube";
 
 export interface Ep {
-  frontmatter: FMType
-  body: any
+  frontmatter: FMType;
+  body: any;
 }
 
 interface EpisodeProps {
-  episode: string
+  episode: string;
 }
 
-const Episode = ({episode}) => {
-  const ep: Ep = JSON.parse(episode)
+const Episode = ({ episode }) => {
+  const ep: Ep = JSON.parse(episode);
   return (
     <Box gap="medium">
       <Head
@@ -39,8 +39,22 @@ const Episode = ({episode}) => {
 
       <Box direction="row-responsive" wrap flex="grow">
         <Listen />
-        <Box align="center" flex gap="xsmall" pad={{horizontal: 'medium'}}>
+        <Box align="center" flex gap="xsmall" pad={{ horizontal: "medium" }}>
           <DownloadBar frontmatter={ep.frontmatter} />
+          {ep.frontmatter.episode >= 146 && (
+            <div
+              style={{
+                fontSize: 20,
+                lineHeight: "1.5em",
+                marginBottom: 20,
+              }}
+            >
+              <a href="https://patreon.com/happyhour">
+                Become a Patron at the Gin Martinis tier to watch the video
+                recording of this episode.
+              </a>
+            </div>
+          )}
           {ep.frontmatter.youtube ? (
             <YouTube id={ep.frontmatter.youtube} />
           ) : (
@@ -51,24 +65,26 @@ const Episode = ({episode}) => {
       </Box>
       <Footer />
     </Box>
-  )
-}
+  );
+};
 
-export const getStaticProps: GetStaticProps = async ({params: {episode}}) => {
+export const getStaticProps: GetStaticProps = async ({
+  params: { episode },
+}) => {
   return {
     props: {
       episode: JSON.stringify(
-        contents.find((content) => content.frontmatter.slug === episode)
+        contents.find((content) => content.frontmatter.slug === episode),
       ),
     },
-  }
-}
+  };
+};
 
 export const getStaticPaths: GetStaticPaths = async () => ({
   paths: contents.map((content) => ({
-    params: {episode: content.frontmatter.slug},
+    params: { episode: content.frontmatter.slug },
   })),
   fallback: false,
-})
+});
 
-export default Episode
+export default Episode;
